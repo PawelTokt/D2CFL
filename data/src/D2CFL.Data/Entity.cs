6cf0588da697f7ceeb683e7cb155b14e7bbe1370
WS-TEMP-GRODNO-\pavel.tokt <polltptokc@gmail.com>
2018-03-22 13:33:04 +0300

    add class Entity

---
+++ data/src/D2CFL.Data/Entity.cs
@@ -0,0 +1,23 @@
+﻿using D2CFL.Data.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace D2CFL.Data
+{
+    /// <summary>
+    /// Entity for data layer.
+    /// </summary>
+    /// <seealso>
+    ///     <cref>D2CFL.Data.Interfaces.IEntity</cref>
+    /// </seealso>
+    public class Entity : IEntity
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>Int number.</value>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
