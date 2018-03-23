9f4aa75650fcf5e73edb33a23c9a8855dfb45276
WS-TEMP-GRODNO-\pavel.tokt <polltptokc@gmail.com>
2018-03-23 11:50:40 +0300

    add Initial Migration for BaseContext

---
+++ database/D2CFL.Migrations/Migrations/20180323084755_Initial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace D2CFL.Migrations.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
