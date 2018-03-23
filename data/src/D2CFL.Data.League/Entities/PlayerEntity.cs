038a3f03f32a3b0505aba281cb6169c07e94cb02
WS-TEMP-GRODNO-\pavel.tokt <polltptokc@gmail.com>
2018-03-23 16:08:32 +0300

    add several base entities

---
+++ data/src/D2CFL.Data.League/Entities/PlayerEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace D2CFL.Data.League.Entities
+{
+    public class PlayerEntity : Entity
+    {
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+        public string Surname { get; set; }
+
+        public int? PositionEntityId { get; set; }
+        public PositionEntity Position { get; set; }
+
+        public int? TeamEntityId { get; set; }
+        public TeamEntity Team { get; set; }
+    }
+}
---
+++ data/src/D2CFL.Data.League/Entities/PositionEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace D2CFL.Data.League.Entities
+{
+    public class PositionEntity : Entity
+    {
+        public string Name { get; set; }
+        public double KillCoefficient { get; set; }
+        public double AssistCoefficient { get; set; }
+        public double DeathCoefficient { get; set; }
+
+        public virtual ICollection<PlayerEntity> Players { get; set; }
+    }
+}
