b2e0d6e85215625c6b568f7d225a7f9f9170055d
WS-TEMP-GRODNO-\pavel.tokt <polltptokc@gmail.com>
2018-03-26 16:50:03 +0300

    add autofac DataLeagueMoule for registration interfaces of repositories with their realization

---
+++ data/src/D2CFL.Data.League/DataLeagueModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using D2CFL.Data.Interfaces;
+using D2CFL.Data.League.Entities;
+
+namespace D2CFL.Data.League
+{
+    public class DataLeagueModule : Module
+    {
+        protected override void Load(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<Repository<PlayerEntity>>().As<IRepository<PlayerEntity>>()
+                .InstancePerLifetimeScope();
+            containerBuilder.RegisterType<Repository<PositionEntity>>().As<IRepository<PositionEntity>>()
+                .InstancePerLifetimeScope();
+            containerBuilder.RegisterType<Repository<TeamEntity>>().As<IRepository<TeamEntity>>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
