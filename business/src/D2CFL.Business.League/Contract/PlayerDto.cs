2f5242b4ee8dfc8e351262e1b3690c8949711132
WS-TEMP-GRODNO-\pavel.tokt <polltptokc@gmail.com>
2018-03-26 17:50:59 +0300

    add Id property for DtoModels(Business.League) and ViewModel(WebSite.Admin)

--- business/src/D2CFL.Business.League/Contract/PlayerDto.cs
+++ business/src/D2CFL.Business.League/Contract/PlayerDto.cs
@@ -2,6 +2,7 @@
 {
     public class PlayerDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Nickname { get; set; }
         public string Surname { get; set; }
--- business/src/D2CFL.Business.League/Contract/TeamDto.cs
+++ business/src/D2CFL.Business.League/Contract/TeamDto.cs
@@ -2,6 +2,7 @@
 {
     public class TeamDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Region { get; set; }
     }
