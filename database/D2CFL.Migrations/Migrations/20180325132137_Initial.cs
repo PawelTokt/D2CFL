0f033799585e68bb6ba82e395b8e2c1672a94a8a
WS-TEMP-GRODNO-\pavel.tokt <polltptokc@gmail.com>
2018-03-26 10:35:57 +0300

    cleaning code

--- database/D2CFL.Migrations/Migrations/20180325132137_Initial.cs
+++ database/D2CFL.Migrations/Migrations/20180325132137_Initial.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
-using System;
-using System.Collections.Generic;
 
 namespace D2CFL.Migrations.Migrations
 {
--- database/D2CFL.Migrations/Migrations/League/LeagueContextModelSnapshot.cs
+++ database/D2CFL.Migrations/Migrations/League/LeagueContextModelSnapshot.cs
@@ -3,10 +3,6 @@ using D2CFL.Migrations.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.EntityFrameworkCore.Storage.Internal;
-using System;
 
 namespace D2CFL.Migrations.Migrations.League
 {
