f2f613fb3197b79e4e15b53bd3f026a1326aa823
WS-TEMP-GRODNO-\pavel.tokt <polltptokc@gmail.com>
2018-03-26 17:46:56 +0300

    Add constructors for PositionEntity and TeamEntity

--- data/src/D2CFL.Data.League/Contract/PositionEntity.cs
+++ data/src/D2CFL.Data.League/Contract/PositionEntity.cs
@@ -4,6 +4,12 @@ namespace D2CFL.Data.League.Contract
 {
     public class PositionEntity : Entity
     {
+        public PositionEntity()
+        {
+            // ReSharper disable once VirtualMemberCallInConstructor
+            Players = new List<PlayerEntity>();
+        }
+
         public string Name { get; set; }
         public double KillCoefficient { get; set; }
         public double DeathCoefficient { get; set; }
--- data/src/D2CFL.Data.League/Contract/TeamEntity.cs
+++ data/src/D2CFL.Data.League/Contract/TeamEntity.cs
@@ -4,6 +4,12 @@ namespace D2CFL.Data.League.Contract
 {
     public class TeamEntity : Entity
     {
+        public TeamEntity()
+        {
+            // ReSharper disable once VirtualMemberCallInConstructor
+            Players = new List<PlayerEntity>();
+        }
+
         public string Name { get; set; }
         public string Region { get; set; }
 
