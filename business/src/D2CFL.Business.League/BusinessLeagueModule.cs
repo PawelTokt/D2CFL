1e277641f82d28af955580b06e57359604f576a7
pavel.tokt <polltptokc@gmail.com>
2018-03-27 14:52:30 +0300

    add BusinessLeagueModule for autofac

---
+++ business/src/D2CFL.Business.League/BusinessLeagueModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using D2CFL.Data.League;
+using D2CFL.Data.League.Contract;
+using Microsoft.EntityFrameworkCore;
+
+namespace D2CFL.Business.League
+{
+    public class BusinessLeagueModule : Module
+    {
+        private readonly DbContextOptions _dbContextOptions;
+        private readonly string _schemaName;
+
+
+        protected override void Load(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<LeagueUnitOfWork>().As<ILeagueUnitOfWork>().InstancePerLifetimeScope();
+        }
+    }
+}
