3a77dc94ef38ad2343170c7aaea0eb7099d58431
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-03-29 22:37:03 +0300

    Created PlayerController. Move methods from HomeController to PlayerController

--- website/src/D2CFL.WebSite.Admin/Controllers/HomeController.cs
+++ website/src/D2CFL.WebSite.Admin/Controllers/HomeController.cs
@@ -1,25 +1,12 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
-using D2CFL.WebSite.Admin.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using D2CFL.Business.League.Contract;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace D2CFL.WebSite.Admin.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IPlayerService _playerService;
-        
-        public HomeController(IPlayerService playerService)
+        public IActionResult Index()
         {
-            _playerService = playerService;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            var players = Mapper.Map<IList<PlayerDto>, List<PlayerViewModel>>(await _playerService.GetList());
-            return View(players);
+            return View();
         }
     }
 }
---
+++ website/src/D2CFL.WebSite.Admin/Controllers/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using D2CFL.Business.League.Contract;
+using D2CFL.WebSite.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D2CFL.WebSite.Admin.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly IPlayerService _playerService;
+
+        public PlayerController(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var players = Mapper.Map<IList<PlayerDto>, List<PlayerViewModel>>(await _playerService.GetList());
+            return View(players);
+        }
+
+        public async Task<IActionResult> Update()
+        {
+            return View();
+        }
+    }
+}
