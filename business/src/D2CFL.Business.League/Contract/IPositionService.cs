e8c87a8414becfab15b88d4950f8ded5502928a3
pavel.tokt <polltptokc@gmail.com>
2018-04-03 18:33:40 +0300

    solved PositionService solved PositionController

--- business/src/D2CFL.Business.League/Contract/IPositionService.cs
+++ business/src/D2CFL.Business.League/Contract/IPositionService.cs
@@ -6,7 +6,7 @@ namespace D2CFL.Business.League.Contract
     public interface IPositionService
     {
         Task<IList<PositionDto>> GetList();
-        Task<PositionDto> Get();
+        Task<PositionDto> Get(int id);
         Task Insert(PositionDto playerDto);
         void Update(PositionDto playerDto);
         void Delete(PositionDto playerDto);
--- business/src/D2CFL.Business.League/PositionService.cs
+++ business/src/D2CFL.Business.League/PositionService.cs
@@ -1,4 +1,6 @@
-﻿using D2CFL.Data.Interfaces;
+﻿using AutoMapper;
+using System.Linq;
+using D2CFL.Data.Interfaces;
 using System.Threading.Tasks;
 using D2CFL.Data.League.Contract;
 using System.Collections.Generic;
@@ -21,24 +23,32 @@ namespace D2CFL.Business.League
             return await _leagueUnitOfWork.PositionRepository.GetListAsync<PositionDto>(_dataMapper);
         }
 
-        public Task<PositionDto> Get()
+        public async Task<PositionDto> Get(int id)
         {
-            throw new System.NotImplementedException();
+            var positions = await _leagueUnitOfWork.PositionRepository.GetListAsync<PositionDto>(_dataMapper);
+
+            return positions.FirstOrDefault(x => x.Id == id);
         }
 
-        public Task Insert(PositionDto playerDto)
+        public async Task Insert(PositionDto positionDto)
         {
-            throw new System.NotImplementedException();
+            await _leagueUnitOfWork.PositionRepository.InsertAsync(Mapper.Map<PositionDto, PositionEntity>(positionDto));
+
+            await _leagueUnitOfWork.SaveAsync();
         }
 
-        public void Update(PositionDto playerDto)
+        public void Update(PositionDto positionDto)
         {
-            throw new System.NotImplementedException();
+            _leagueUnitOfWork.PositionRepository.Update(Mapper.Map<PositionDto, PositionEntity>(positionDto));
+
+            _leagueUnitOfWork.Save();
         }
 
-        public void Delete(PositionDto playerDto)
+        public void Delete(PositionDto positionDto)
         {
-            throw new System.NotImplementedException();
+            _leagueUnitOfWork.PositionRepository.Delete(Mapper.Map<PositionDto, PositionEntity>(positionDto));
+
+            _leagueUnitOfWork.Save();
         }
     }
 }
