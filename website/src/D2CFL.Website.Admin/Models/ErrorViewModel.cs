9e4009437f6e8ed6a7b1d24322aea1584dc85bf3
pavel.tokt <polltptokc@gmail.com>
2018-04-10 17:25:30 +0300

    added project D2CFL.Website.Admin

---
+++ website/src/D2CFL.Website.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace D2CFL.Website.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
